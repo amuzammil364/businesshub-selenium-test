2e3b65de798cc92b862b0de27e5714e1935ce50a
amuzammil364 <jemniekk@gmail.com>
2022-09-16 12:33:11 +0500

    Assertion and xPath

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,20 +1,39 @@
 require('chromedriver');
 const webdriver = require('selenium-webdriver');
+// Assert
+const should = require('chai').should();
 
 const test_example = async ()=>{
     
-    //opening chrome
-    let driver = await new webdriver.Builder().forBrowser('chrome').build();
+    /**
+     * Variables
+     */
+    let username = 'msquare';
+    let password = 'CGEzumf4lAk4q1onpP4tQyO'
 
-    //reaching the Destination
-    await driver.get('https://dev.businesshub.co.kr/admin/');
 
     /**
      * perform actions 
      */
-    // --filling inputs
-    await driver.findElement(webdriver.By.name('log')).sendKeys('Learn Selenium');
 
+    //opening chrome
+    let driver = await new webdriver.Builder().forBrowser('chrome').build();
+
+    //reaching the Destination
+    await driver.get('https://businesshub.co.kr/login/');
+
+    // --filling username
+    await driver.findElement(webdriver.By.name('log')).sendKeys(username);
+
+    // --filling Password
+    await driver.findElement(webdriver.By.name('pwd')).sendKeys(password,webdriver.Key.RETURN);
+
+    // --cheking element text to verify if user logged in or not (ASSERTION)
+    let loggedInUser = await driver.findElement(webdriver.By.xpath('//*[@id="wp-admin-bar-my-account"]/a/span')).getText().then((value)=>{
+        return value;
+    });
+    // (ASSERTION)
+    loggedInUser.should.equal(username);
 
     //close the browser
     await driver.quit();
